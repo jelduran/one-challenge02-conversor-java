b4f41dc6ad73280175117c9734102579818bb201
John Elvis Durán <jelduran@gmail.com>
2023-08-30 01:32:46 -0500

    Agregar atributos a ConversorView

--- src/com/one/challenge/conversor/view/ConversorView.java
+++ src/com/one/challenge/conversor/view/ConversorView.java
@@ -4,4 +4,11 @@ import javax.swing.JOptionPane;
 
 public class ConversorView extends JOptionPane{
 
+	private static final long serialVersionUID = 9027372087370487375L;
+	
+	private String[] opcionesConversion;
+	private String[] conversionesMoneda;
+	private String[] conversionesCripto;
+	private String[] conversionesStock;
+
 }
