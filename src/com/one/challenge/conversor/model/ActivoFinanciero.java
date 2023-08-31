d39a597eacd6676d90873a9aacdae03c68ce9243
John Elvis Durán <jelduran@gmail.com>
2023-08-30 22:21:27 -0500

    Definir atributos y constructores a ActivoFinanciero
    Se definen los atributos y se implementan 2 constructores, uno por
    defecto y otro sobrecargado para instanciar activos financieros de
    acuerdo a los argumentos recibidos.

--- src/com/one/challenge/conversor/model/ActivoFinanciero.java
+++ src/com/one/challenge/conversor/model/ActivoFinanciero.java
@@ -1,5 +1,28 @@
 package com.one.challenge.conversor.model;
 
 public class ActivoFinanciero {
+	
+	private String tipo;
+	private String codigo;
+	private char simbolo;
+	private double precio;
+	private ActivoFinanciero activoRef;
+
+	public ActivoFinanciero() {
+		this.tipo = "Moneda";
+		this.codigo = "USD";
+		this.simbolo = '$';
+		this.precio = 1;
+		this.activoRef = this;
+	}
+
+	public ActivoFinanciero(String tipo, String codigo, char simbolo, double precio,
+			ActivoFinanciero activoFinanciero) {
+		this.tipo = tipo;
+		this.codigo = codigo;
+		this.simbolo = simbolo;
+		this.precio = activoFinanciero != null ? precio : 0.0;
+		this.activoRef = activoFinanciero != null ? activoFinanciero : null;
+	}
 
 }
