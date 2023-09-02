eb09167da72cbfeb80c6d0026bb9fa47747e20de
John Elvis Durán <jelduran@gmail.com>
2023-09-01 20:02:29 -0500

    Implementar constructor por defecto ActivoCriptomoneda y ActivoStock

--- src/com/one/challenge/conversor/model/ActivoCriptomoneda.java
+++ src/com/one/challenge/conversor/model/ActivoCriptomoneda.java
@@ -3,7 +3,11 @@ package com.one.challenge.conversor.model;
 public class ActivoCriptomoneda extends ActivoFinanciero {
 
 	public ActivoCriptomoneda() {
-		
+		this.tipo = "criptomoneda";
+		this.codigo = "BTC";
+		this.simbolo = '\u20BF';
+		this.precio = 25743.20;
+		this.activoRef = new ActivoMoneda();
 	}
 
 	public ActivoCriptomoneda(String codigo, char simbolo, double precio, ActivoFinanciero activoFinanciero) {
--- src/com/one/challenge/conversor/model/ActivoStock.java
+++ src/com/one/challenge/conversor/model/ActivoStock.java
@@ -3,7 +3,11 @@ package com.one.challenge.conversor.model;
 public class ActivoStock extends ActivoFinanciero {
 
 	public ActivoStock() {
-		// TODO Auto-generated constructor stub
+		this.tipo = "stock";
+		this.codigo = "TSLA";
+		this.simbolo = '\u2191';
+		this.precio = 245.01;
+		this.activoRef = new ActivoMoneda();
 	}
 
 	public ActivoStock(String codigo, char simbolo, double precio, ActivoFinanciero activoFinanciero) {
