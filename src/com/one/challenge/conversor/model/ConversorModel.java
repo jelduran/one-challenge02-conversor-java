8b19078cff9d2707070d5542c18a3eb523005e31
John Elvis Durán <jelduran@gmail.com>
2023-09-03 02:32:58 -0500

    Agregar atributos a ConversorModel

--- src/com/one/challenge/conversor/model/ConversorModel.java
+++ src/com/one/challenge/conversor/model/ConversorModel.java
@@ -2,4 +2,9 @@ package com.one.challenge.conversor.model;
 
 public class ConversorModel {
 
+	ActivoFinanciero[] monedas;
+	ActivoFinanciero[] criptomonedas;
+	ActivoFinanciero[] stocks;
+	
+	ActivoFinanciero activoBase;
 }
