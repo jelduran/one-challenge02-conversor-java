0ac994c74037ab469c6da91937a942a21349f94b
John Elvis Durán <jelduran@gmail.com>
2023-09-05 13:47:51 -0500

    Eliminar herencia para ConversorView
    Herencia de JOPtionPane removida para ConversorView ya que muchos de los
    metodos de JOptionPane son estaticos.

--- src/com/one/challenge/conversor/view/ConversorView.java
+++ src/com/one/challenge/conversor/view/ConversorView.java
@@ -1,16 +1,12 @@
 package com.one.challenge.conversor.view;
 
-import javax.swing.JOptionPane;
+public class ConversorView {
 
-public class ConversorView extends JOptionPane{
-
-	private static final long serialVersionUID = 9027372087370487375L;
-	
 	private String[] opcionesConversion;
 	private String[] conversionesMoneda;
 	private String[] conversionesCripto;
 	private String[] conversionesStock;
-	
+
 	public ConversorView() {
 		opcionesConversion = new String[] { "Monedas", "Criptos", "Stocks" };
 		conversionesMoneda = new String[] { "COP a USD", "COP a EUR", "COP a GBP", "COP a JPY", "COP a KRW",
@@ -18,7 +14,7 @@ public class ConversorView extends JOptionPane{
 		conversionesCripto = new String[] { "BTC a ETH", "BTC a USDT", "BTC a BNB", "BTC a XRP", "ETH a BTC",
 				"USDT a BTC", "BNB a BTC", "XRP a BTC" };
 		conversionesStock = new String[] { "USD a TSLA", "USD a NVDA", "USD a NKE", "USD a DUFN", "TSLA a USD",
-				"NVDA a USD", "NKE a USD", "DUFN a USD"};
+				"NVDA a USD", "NKE a USD", "DUFN a USD" };
 	}
 
 	public String[] getOpcionesConversion() {
